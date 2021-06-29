c20868df141fc1d43f05136d5a04c3e4f1483f9d
tm <mykolyk@stuycs.org>
2021-06-29 11:40:50 -0400

    +increment tester

---
+++ daily/1/Tiesto.java
@@ -0,0 +1,20 @@
+public class Tiesto {
+
+  public static void main( String args[] ) {
+
+    int x;
+
+    //leave exactly one of these uncommented
+    //x = +1;
+    x = -1;
+
+    //jot down predictions,
+    // run
+    // reconcile output with expectation...
+    System.out.println(x);
+    System.out.println(x++);
+    System.out.println(x++);
+    System.out.println(--x);
+    System.out.println(--x);
+  }
+}
