8baba2357cdc9a2c08488bd1f96265c88b2bf1d7
tm <mykolyk@stuycs.org>
2021-07-01 10:19:48 -0400

    +2d arr work

---
+++ daily/3/arr2d/skel/TwoDimArray.java
@@ -0,0 +1,74 @@
+/** skeleton file for
+ class twoDimArray
+
+  ...practice working with 2D arrays
+*/
+
+public class TwoDimArray
+{
+
+  //print each row of 2D integer array a on its own line,
+  // using a FOR loop
+  public static void print1( int[][] a ) {
+
+  }
+
+
+  //print each row of 2D integer array a on its own line,
+  // using a FOREACH loop
+  public static void print2( int[][] a ) {
+
+  }
+
+
+  //return sum of all items in 2D integer array a
+  public static int sum1( int[][] a ) {
+
+  }
+
+
+  //return sum of all items in 2D integer array a
+  // using helper fxn sumRow
+  public static int sum2( int [][] m ) {
+
+  }
+
+
+  //return sum of all items on row r of 2D integer array a
+  // using a FOR loop
+  public static int sumRow( int r, int[][] a ) {
+
+  }
+
+
+  //return sum of all items on row r of 2D integer array a
+  // using a FOREACH loop
+  public static int sumRow2( int r, int[][] m ) {
+
+  }
+
+
+  public static void main( String [] args )
+  {
+		/* ~~~v~~~~~slide~me~down~as~you~test~~~~~~~~~~~~~~~v~~~
+       int [][] m1 = new int[4][2];
+       int [][] m2 = { {2,4,6}, {3,5,7} };
+       int [][] m3 = { {2}, {4,6}, {1,3,5} };
+       print1(m1);
+       print1(m2);
+       print1(m3);
+       print2(m1);
+       print2(m2);
+       print2(m3);
+       System.out.print("testing sum1...\n");
+       System.out.println("sum m1 : " + sum1(m1));
+       System.out.println("sum m2 : " + sum1(m2));
+       System.out.println("sum m3 : " + sum1(m3));
+       System.out.print("testing sum2...\n");
+       System.out.println("sum m1 : " + sum2(m1));
+       System.out.println("sum m2 : " + sum2(m2));
+       System.out.println("sum m3 : " + sum2(m3));
+       ~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~*/
+  }//end main()
+
+}//end class TwoDimArray
---
+++ daily/3/readme.md
@@ -0,0 +1,4 @@
+
+PROTIP: Copy this `skel` dir to your working dir, then copy the skeleton inside it to your working dir and grow it. This way you will always have the skeleton on hand if you need it...
+
+
