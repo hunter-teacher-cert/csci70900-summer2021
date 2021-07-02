60cbb14de76ae87984a2f94f3fa234450bf04821
tm <mykolyk@stuycs.org>
2021-07-02 18:04:46 -0400

    +time stub

---
+++ daily/5/Driver.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver {
+
+  public static void main(String[] args) {
+
+    //declare a var of appropriate type to assign an instance of Time to
+    Time t;
+
+    //assign var the address of a newly-apportioned Time object
+    t = new Time();
+  }//end main()
+
+}//end class
---
+++ daily/5/Time.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+
+public class Time {
+
+  // your implementation of class Time here
+
+}//end class
