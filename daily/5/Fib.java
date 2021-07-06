294c6f5c7305d1847499a38560dfff9a98174448
tm <mykolyk@stuycs.org>
2021-07-06 07:47:43 -0400

    +fib tester

---
+++ daily/5/Fib.java
@@ -0,0 +1,26 @@
+// heading
+
+public class Fib {
+
+  //your implementation of fib(n) here
+
+
+  public static void main( String[] args ) {
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    System.out.println( fib(0) ); // -> 0
+    System.out.println( fib(1) ); // -> 1
+    System.out.println( fib(2) ); // -> 1
+    System.out.println( fib(3) ); // -> 2
+    System.out.println( fib(4) ); // -> 3
+    System.out.println( fib(5) ); // -> 5
+
+    //now go big:
+    System.out.println( fib(10) ); // -> 55
+    System.out.println( fib(20) ); // -> 6765
+    System.out.println( fib(40) ); // -> 102334155
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+  }//end main()
+
+}//end class Fib
